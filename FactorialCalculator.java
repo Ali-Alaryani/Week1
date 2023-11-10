28882663c77e0d5a78ec125114e61dbfd9745978
Ali-Alaryani <150218683+Ali-Alaryani@users.noreply.github.com>
2023-11-10 17:58:09 +0400

    Add files via upload

---
+++ FactorialCalculator.java
@@ -0,0 +1,28 @@
+
+public class FactorialCalculator {
+
+	public static void main(String[] args) {
+	       Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int number = scanner.nextInt();
+
+	        long factorial = calculateFactorial(number);
+
+	        System.out.println("Factorial of " + number + " is: " + factorial);
+
+	        scanner.close();
+	    }
+
+	    private static long calculateFactorial(int n) {
+	        if (n < 0) {
+	            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+	        }
+
+	        if (n == 0 || n == 1) {
+	            return 1;
+	        } else {
+	            return n * calculateFactorial(n - 1);
+	        }
+	    }
+	}
